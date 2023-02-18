30e68c90f8c47bdd844a6e50234a4b0950b9d316
maxim100000 <max1001@yandex.ru>
2023-02-18 10:00:24 +0700

    deleted

--- news_paper/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Author, Category, Comment
-
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Comment)
